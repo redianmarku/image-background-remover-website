c0a76182bd1d2c9710fdf36715191a04d9b3b35d
TopNotch Programmer <42848358+redianmarku@users.noreply.github.com>
2024-08-13 13:28:34 +0200

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,6 @@
+import os
 from app import app
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 4000))  # Get the port from environment or use 5000 for local
+    app.run(host="0.0.0.0", port=port)
